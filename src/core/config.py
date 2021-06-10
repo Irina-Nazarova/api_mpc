5756ba2e3869a6b33fc8695c3b7c33dc5738b0de
Yury <yury.s.nazarov@gmail.com>
2021-06-10 21:29:27 +0300

    Sys: conf has been fix

--- src/core/config.py
+++ src/core/config.py
@@ -4,7 +4,7 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     SERVER_HOST: str = '127.0.0.1'
     SERVER_PORT: int = 8000
-    PG_DATABASE_URL: str = "postgresql://api_mpc:api_mpc_pwd@localhost:54321/api_mpc"
+    PG_DATABASE_URL: str = "postgresql://api_mpc:api_mpc_pwd@localhost:54321/api_mpc_db"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
     ALGORITHM: str = "HS256"
     # Below default key or "openssl rand -hex 32".
